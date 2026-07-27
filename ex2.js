6cad7542822fafc03ed5f56fc284410444718678
Mansi Mittal <mittalmansi05@gmail.compwdgit--versiongit config --global user.email mittalmansi05@gmail.com>
2026-07-27 15:18:28 +0530

    demo filter map reduce

---
+++ ex2.js
@@ -0,0 +1,10 @@
+const num = [12, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const even = num.filter((i) => i % 2 === 0);
+console.log("num =", num);
+console.log("even =", even);
+
+const square = num.map((i) => i * i);
+console.log("square =", square);
+const sum = square.reduce((s, i) => (s, i))
+console.log("sun=",sum);
